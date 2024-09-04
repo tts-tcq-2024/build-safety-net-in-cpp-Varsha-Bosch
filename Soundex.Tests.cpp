90759f4d60b229a7b1b266aaf3a22bc063454b69
Varsha-Bosch <varshasrinivas46@gmail.com>
2024-09-04 10:25:16 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,3 +10,12 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesDifferentStartingCharacterSameSoundex) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+}
+     
+TEST(SoundexTest, HandleLongName){
+    EXPECT_EQ(generateSoundex("Ashcraft"),"A261");
+   EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+}
